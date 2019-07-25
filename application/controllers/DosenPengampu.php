28f6dead13eb636b049d9e076465be30beec23d8
Novalita <novalita.k.wardhana@gmail.com>
2019-07-25 21:21:01 +0700

    Dosen pengampu controller

--- application/controllers/DosenPengampu.php
+++ application/controllers/DosenPengampu.php
@@ -1,212 +1,208 @@
 <?php
-    class DosenPengampu extends CI_Controller {
+class DosenPengampu extends CI_Controller {
 
-        private $dosenPengampuModel;
-        private $dosenModel;
+	private $dosenPengampuModel;
+	private $dosenModel;
 
-        public function __construct() {
-            parent::__construct();
-            if($this->session->userdata('status') != "login"){
-				redirect(base_url("Auth"));
+	public function __construct() {
+		parent::__construct();
+		if($this->session->userdata('status') != "login"){
+			redirect(base_url("Auth"));
+		}
+		$this->load->library('session');
+
+		$this->load->model('DosenPengampuModel');
+		$this->dosenPengampuModel=$this->DosenPengampuModel;
+
+		$this->load->model('DosenModel');
+		$this->dosenModel=$this->DosenModel;
+	}
+
+	public function index() {
+		$this->load->view('masterDosenPengampu/read');
+	}
+
+	public function getListDosen() {
+		$columns = array( 
+			0 =>'nomor', 
+			1 =>'aksi', 
+			2 =>'nik',
+			3=> 'nama',
+		);
+			
+		//Get total data
+		$totalData = $this->dosenModel->getTotalData();
+			
+		$limit = $_POST['length'];
+		$start = $_POST['start'];
+		$order = 'id';
+		$dir = 'desc';
+		$search=$_POST['search']['value'];
+
+		$params=array(
+			'limit' => $limit,
+			'start' => $start,
+			'order' => $order,
+			'dir' => $dir,
+			'search' => $search
+		);
+
+		$getListDosen=$this->dosenPengampuModel->getListDosen($params);
+		$totalFiltered=$this->dosenPengampuModel->getListDosenCount($params);
+			
+		$data = array();
+		if(!empty($getListDosen)) {
+			foreach ($getListDosen as $row){
+				$nestedData['nomor'] = $row->nomor;
+				$nestedData['aksi'] = "
+					<a href='".base_url('DosenPengampu/detail?id=').$row->id."'>
+						<button class='btn btn-sm btn-primary'><i class='fa fa-search-plus'></i></button>
+						</a>
+					";
+				$nestedData['nik'] = $row->nik;
+				$nestedData['nama'] = $row->nama;
+				$nestedData['jumlah_matkul'] = $row->jumlah_mata_kuliah;
+				$data[] = $nestedData;
 			}
-            $this->load->library('session');
+		}
+			
+		$json_data = array(
+			"draw"            => intval($_POST['draw']),  
+			"recordsTotal"    => intval($totalData),  
+			"recordsFiltered" => intval($totalFiltered), 
+			"data"            => $data   
+			);
+	
+		echo json_encode($json_data);
+	}
 
-            $this->load->model('DosenPengampuModel');
-            $this->dosenPengampuModel=$this->DosenPengampuModel;
+	public function detail() {
+		$id=$_GET['id'];
+		$dataDosen=$this->dosenModel->getListDosenById($id);
+		$dataMataKuliah=$this->dosenPengampuModel->getListMataKuliah();
+		$dataKelas=$this->dosenPengampuModel->getListKelas();
 
-            $this->load->model('DosenModel');
-            $this->dosenModel=$this->DosenModel;
-        }
+		$data=[];
+		$data['dataDosen']=$dataDosen;
+		$data['dataMataKuliah']=$dataMataKuliah;
+		$data['dataKelas']=$dataKelas;
 
-        public function index() {
-            $this->load->view('masterDosenPengampu/read');
-        }
+		$this->load->view('masterDosenPengampu/readDetail', $data);
+	}
 
-        public function getListDosen() {
-            $columns = array( 
-                0 =>'nomor', 
-                1 =>'aksi', 
-                2 =>'nik',
-                3=> 'nama',
-            );
-            
-            //Get total data
-            $totalData = $this->dosenModel->getTotalData();
-            
-            $limit = $_POST['length'];
-            $start = $_POST['start'];
-            $order = 'id';
-            $dir = 'desc';
-            $search=$_POST['search']['value'];
+	public function create() {
+		$params=$_POST;
+		$pathUrl='DosenPengampu/detail?id='.$_POST['id_dosen'];
+		$hasil=$this->dosenPengampuModel->create($params);
+		if ($hasil===TRUE) {
+			$this->session->set_flashdata('imageMsg', 'create_success');
+			redirect(base_url($pathUrl));
+		} else {
+			$this->session->set_flashdata('imageMsg', 'create_failed');
+			redirect(base_url($pathUrl));
+		}
+	}
 
-            $params=array(
-                'limit' => $limit,
-                'start' => $start,
-                'order' => $order,
-                'dir' => $dir,
-                'search' => $search
-            );
+	public function getListDosenPengampu() {
+		$columns = array( 
+			0 =>'nomor', 
+			1 =>'aksi', 
+			2 =>'kode_mata_kuliah',
+			3 => 'mata_kuliah',
+			4 => 'kelas',
+			5 => 'jam_mulai',
+			6 => 'jam_selesai',
+			7 => 'ruang',
+		);
 
-            $getListDosen=$this->dosenPengampuModel->getListDosen($params);
-            $totalFiltered=$this->dosenPengampuModel->getListDosenCount($params);
-           
-            $data = array();
-	        if(!empty($getListDosen)) {
-	            foreach ($getListDosen as $row)
-	            {
+		//Get total data
+		$totalData = $this->dosenPengampuModel->getTotalData($_POST['id_dosen']);
 
-	                $nestedData['nomor'] = $row->nomor;
-	                $nestedData['aksi'] = "
-                        	<a href='".base_url('dosenPengampu/detail?id=').$row->id."'>
-	                          <button class='btn btn-sm btn-primary'><i class='fa fa-search-plus'></i></button>
-                            </a>
-                        	";
-	                $nestedData['nik'] = $row->nik;
-                    $nestedData['nama'] = $row->nama;
-                    $nestedData['jumlah_matkul'] = $row->jumlah_mata_kuliah;
-	                $data[] = $nestedData;
-	            }
-            }
-            
-            $json_data = array(
-                "draw"            => intval($_POST['draw']),  
-                "recordsTotal"    => intval($totalData),  
-                "recordsFiltered" => intval($totalFiltered), 
-                "data"            => $data   
-                );
-        
-            echo json_encode($json_data);
-        }
+		$limit = $_POST['length'];
+		$start = $_POST['start'];
+		$order = 'id';
+		$dir = 'desc';
+		$search=$_POST['search']['value'];
 
-        public function detail() {
-            $id=$_GET['id'];
-            $dataDosen=$this->dosenModel->getListDosenById($id);
-            $dataMataKuliah=$this->dosenPengampuModel->getListMataKuliah();
-            $dataKelas=$this->dosenPengampuModel->getListKelas();
+		$params=array(
+			'limit' => $limit,
+			'start' => $start,
+			'order' => $order,
+			'dir' => $dir,
+			'search' => $search,
+			'id_dosen' => $_POST['id_dosen']
+		);
 
-            $data=[];
-            $data['dataDosen']=$dataDosen;
-            $data['dataMataKuliah']=$dataMataKuliah;
-            $data['dataKelas']=$dataKelas;
+		$getListDosenPengampu=$this->dosenPengampuModel->getListDosenPengampu($params);
+		$totalFiltered=$this->dosenPengampuModel->getListDosenPengampuCount($params);
 
-            $this->load->view('masterDosenPengampu/readDetail', $data);
-        }
+		$data = array();
+		if(!empty($getListDosenPengampu)) {
+			foreach ($getListDosenPengampu as $row) {
+				$nestedData['nomor'] = $row->nomor;
+				$nestedData['aksi'] = "
+					<button class='btn btn-sm btn-info' onclick='editRow($row->id)'>
+						<i class='fa fa-pencil'></i>
+					</button>
+					<button class='btn btn-sm btn-danger' data-href='".base_url('DosenPengampu/delete?id=').$row->id."' data-toggle='modal' data-target='#confirm-delete'>
+						<i class='fa fa-trash'></i>
+					</button>
+				";
+				$nestedData['kode_mata_kuliah'] = $row->kode_mata_kuliah;
+				$nestedData['mata_kuliah'] = $row->mata_kuliah;
+				$nestedData['kelas'] = $row->kelas;
+				$nestedData['jam_mulai'] = $row->jam_mulai;
+				$nestedData['jam_selesai'] = $row->jam_selesai;
+				$nestedData['ruang'] = $row->ruang;
+				$data[] = $nestedData;
+			}
+		}
 
-        public function create() {
-            $params=$_POST;
-            $pathUrl='dosenPengampu/detail?id='.$_POST['id_dosen'];
-            $hasil=$this->dosenPengampuModel->create($params);
-            if ($hasil===TRUE) {
-                $this->session->set_flashdata('imageMsg', 'create_success');
-                redirect(base_url($pathUrl));
-            } else {
-                $this->session->set_flashdata('imageMsg', 'create_failed');
-                redirect(base_url($pathUrl));
-            }
-        }
+		$json_data = array(
+			"draw"            => intval($_POST['draw']),  
+			"recordsTotal"    => intval($totalData),  
+			"recordsFiltered" => intval($totalFiltered), 
+			"data"            => $data   
+			);
+	
+		echo json_encode($json_data);
+	}
 
-        public function getListDosenPengampu() {
-            $columns = array( 
-                0 =>'nomor', 
-                1 =>'aksi', 
-                2 =>'kode_mata_kuliah',
-                3 => 'mata_kuliah',
-                4 => 'kelas',
-                5 => 'jam_mulai',
-                6 => 'jam_selesai',
-                7 => 'ruang',
-            );
+	public function delete() {
+		$params=[];
+		$id=$_GET['id'];
+		$params['id']=$id;
 
-            //Get total data
-            $totalData = $this->dosenPengampuModel->getTotalData($_POST['id_dosen']);
+		$dosen=$this->dosenPengampuModel->getDosenId($id);
+		$pathUrl='DosenPengampu/detail?id='.$dosen->id_dosen;
+	
+		$hapusDataDosenPengampu=$this->dosenPengampuModel->delete($params);
+		if ($hapusDataDosenPengampu===TRUE) {
+			$this->session->set_flashdata('imageMsg', 'delete_success');
+			redirect(base_url($pathUrl));
+		} else {
+			$this->session->set_flashdata('imageMsg', 'delete_success');
+			redirect(base_url($pathUrl));
+		}
+	}
 
-            $limit = $_POST['length'];
-            $start = $_POST['start'];
-            $order = 'id';
-            $dir = 'desc';
-            $search=$_POST['search']['value'];
+	public function getDosenPengampuById() {
+		$data = $this->dosenPengampuModel->getDosenPengampuById($_GET['id']);
+		echo json_encode($data);
+	}
 
-            $params=array(
-                'limit' => $limit,
-                'start' => $start,
-                'order' => $order,
-                'dir' => $dir,
-                'search' => $search,
-                'id_dosen' => $_POST['id_dosen']
-            );
-
-            $getListDosenPengampu=$this->dosenPengampuModel->getListDosenPengampu($params);
-            $totalFiltered=$this->dosenPengampuModel->getListDosenPengampuCount($params);
-
-            $data = array();
-            if(!empty($getListDosenPengampu)) {
-	            foreach ($getListDosenPengampu as $row)
-	            {
-	                $nestedData['nomor'] = $row->nomor;
-                    $nestedData['aksi'] = "
-                        <button class='btn btn-sm btn-info' onclick='editRow($row->id)'>
-                            <i class='fa fa-pencil'></i>
-                        </button>
-                        <button class='btn btn-sm btn-danger' data-href='".base_url('dosenPengampu/delete?id=').$row->id."' data-toggle='modal' data-target='#confirm-delete'>
-                            <i class='fa fa-trash'></i>
-                        </button>
-                    ";
-	                $nestedData['kode_mata_kuliah'] = $row->kode_mata_kuliah;
-                    $nestedData['mata_kuliah'] = $row->mata_kuliah;
-                    $nestedData['kelas'] = $row->kelas;
-                    $nestedData['jam_mulai'] = $row->jam_mulai;
-                    $nestedData['jam_selesai'] = $row->jam_selesai;
-                    $nestedData['ruang'] = $row->ruang;
-	                $data[] = $nestedData;
-	            }
-            }
-
-            $json_data = array(
-                "draw"            => intval($_POST['draw']),  
-                "recordsTotal"    => intval($totalData),  
-                "recordsFiltered" => intval($totalFiltered), 
-                "data"            => $data   
-                );
-        
-            echo json_encode($json_data);
-        }
-
-        public function delete() {
-            $params=[];
-            $id=$_GET['id'];
-            $params['id']=$id;
-
-            $dosen=$this->dosenPengampuModel->getDosenId($id);
-            $pathUrl='dosenPengampu/detail?id='.$dosen->id_dosen;
-        
-            $hapusDataDosenPengampu=$this->dosenPengampuModel->delete($params);
-            if ($hapusDataDosenPengampu===TRUE) {
-                $this->session->set_flashdata('imageMsg', 'delete_success');
-                redirect(base_url($pathUrl));
-            } else {
-                $this->session->set_flashdata('imageMsg', 'delete_success');
-                redirect(base_url($pathUrl));
-            }
-        }
-
-        public function getDosenPengampuById() {
-           $data = $this->dosenPengampuModel->getDosenPengampuById($_GET['id']);
-           echo json_encode($data);
-        }
-
-        public function update() {
-            $params=$_POST;
-            $pathUrl='dosenPengampu/detail?id='.$_POST['id_dosen'];
-            $hasil=$this->dosenPengampuModel->update($params);
-            if ($hasil===TRUE) {
-                $this->session->set_flashdata('imageMsg', 'update_success');
-                redirect(base_url($pathUrl));
-            } else {
-                $this->session->set_flashdata('imageMsg', 'delete_failed');
-                redirect(base_url($pathUrl));
-            }
-            
-        }
+	public function update() {
+		$params=$_POST;
+		$pathUrl='DosenPengampu/detail?id='.$_POST['id_dosen'];
+		$hasil=$this->dosenPengampuModel->update($params);
+		if ($hasil===TRUE) {
+			$this->session->set_flashdata('imageMsg', 'update_success');
+			redirect(base_url($pathUrl));
+		} else {
+			$this->session->set_flashdata('imageMsg', 'delete_failed');
+			redirect(base_url($pathUrl));
+		}
+	}
 
 
-    }
+}
