33af362b44fd4da9551d3a33df9c194fcd95b48e
Novalita <novalita.k.wardhana@gmail.com>
2019-10-28 17:19:34 +0700

    Export pdf evaluasi mandiri

--- application/views/evaluasiMandiriExport/export.php
+++ application/views/evaluasiMandiriExport/export.php
@@ -6,9 +6,13 @@
 	<style type="text/css">
 		body { font-family: Times !important; color: #222; }
 		.bagan-cpl { border: 1px solid #789; padding: 10px;margin-top: 5px; margin-bottom: 20px;width: 100% }
-		.tabel-mahasiswa { width: 100% }
-		.tabel-mahasiswa thead th{ padding: 10px 10px; font-size: 12px; vertical-align: top; line-height: 1.8; text-align: justify; }
-   	.tabel-mahasiswa tbody td{ padding: 10px 10px; font-size: 11px; vertical-align: top; line-height: 1.8; text-align: justify; }
+		
+    .tabel-header { width: 100% }
+		.tabel-header thead th{ padding: 8px 6px; font-size: 15px; vertical-align: top; text-align: center; }
+
+    .tabel-mahasiswa { width: 100% }
+		.tabel-mahasiswa thead th{ padding: 8px 6px; font-size: 12px; vertical-align: top; line-height: 1.8; text-align: justify; }
+   	.tabel-mahasiswa tbody td{ padding: 8px 6px; font-size: 11px; vertical-align: top; line-height: 1.8; text-align: justify; }
 
 		.tabel-cpl-detail { width: 100%; border: 1px solid #789 }
 		.tabel-cpl-detail thead { display: table-row-group; }
@@ -27,15 +31,36 @@
 	</style>
 </head>
 <body>
-	<table class="tabel-mahasiswa">
-		<thead>
-			<tr>
-				<th colspan="2" class="text-center">
-					<h2>
-						Laporan Evaluasi Mandiri
-					</h2>
+  
+  <!-- Header evaluasi mahasiswa -->
+  <table class="tabel-header">
+    <thead>
+      <tr>
+        <th rowspan="3" style="width: 15%">
+          <img src="./application/views/evaluasiMandiriExport/logo_uii.png" alt="">
+        </th>
+				<th class="text-center" style="width: 70%">
+					Laporan Evaluasi Mandiri Pengukuran Capaian Pembelajaran
+				</th>
+        <th rowspan="3" style="width: 15%">&nbsp;</th>
+			</tr>
+      <tr>
+				<th class="text-center">
+          Program Studi DIII Analisis Kimia
 				</th>
 			</tr>
+      <tr>
+				<th class="text-center">
+          Universitas Islam Indonesia
+				</th>
+			</tr>
+    </thead>
+  </table>
+
+	<hr>
+
+	<table class="tabel-mahasiswa">
+		<thead>
 			<tr>
 				<th style="width: 12%">Nama</th>
 				<th>: <?php echo $data_mahasiswa->nama ?></th>
